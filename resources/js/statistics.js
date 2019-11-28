6cbe185cdf695414be12c78c8e1249fb6c3489c8
Ichmarah <clitoria_erekta@Clitorias-MacBook-Pro.local>
2019-11-28 11:10:35 +0100

    TGIF prject created

---
+++ resources/js/statistics.js
@@ -0,0 +1,21 @@
+// Object called statistics, consisting of 4 nested objects
+let statistics = {
+
+    "Democrats": {
+        num: 57,
+        votes: 96.97
+    },
+    "Republicans": {
+        num: 46,
+        votes: 88.84
+    },
+    "Independents": {
+        num: 2,
+        votes: 95.18
+    },
+    "Total": {
+        num: 105,
+        votes: "-"
+    }
+
+};
